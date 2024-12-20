19e4193271125e4efa52e5ef313e419dc4f7f061
tentacles <my.tentacles.are.in.you@gmail.com>
2024-12-20 12:15:03 +0100

    tests(file_watcher): add more tests to file watcher

--- tests/spec/ecolog_spec.lua
+++ tests/spec/ecolog_spec.lua
@@ -5,21 +5,21 @@ local stub = require("luassert.stub")
 describe("ecolog", function()
   local ecolog
   local test_dir = vim.fn.tempname()
-  
+
   before_each(function()
     -- Create temp test directory
     vim.fn.mkdir(test_dir, "p")
-    
+
     -- Reset modules
     package.loaded["ecolog"] = nil
     package.loaded["ecolog.utils"] = nil
     package.loaded["ecolog.types"] = nil
     package.loaded["ecolog.shelter"] = nil
-    
+
     -- Load module
     ecolog = require("ecolog")
   end)
-  
+
   after_each(function()
     -- Clean up test directory
     vim.fn.delete(test_dir, "rf")
@@ -30,10 +30,10 @@ describe("ecolog", function()
       ecolog.setup({
         shelter = {
           configuration = {},
-          modules = {}
+          modules = {},
         },
         integrations = {},
-        types = true
+        types = true,
       })
       local config = ecolog.get_config()
       assert.equals(vim.fn.getcwd(), config.path)
@@ -57,17 +57,64 @@ describe("ecolog", function()
         path = test_dir,
         shelter = {
           configuration = {},
-          modules = {}
+          modules = {},
         },
         integrations = {},
-        types = true
+        types = true,
       })
       local env_vars = ecolog.get_env_vars()
-      
+
       assert.is_not_nil(env_vars.DB_HOST)
       assert.equals("localhost", env_vars.DB_HOST.value)
       assert.equals("5432", env_vars.DB_PORT.value)
       assert.equals("secret123", env_vars.API_KEY.value)
+    end)
+  end)
+
+  describe("file watcher", function()
+    local test_dir = vim.fn.tempname()
+    local original_notify
+
+    before_each(function()
+      vim.fn.mkdir(test_dir, "p")
+      -- Store original notify function
+      original_notify = vim.notify
+    end)
+
+    after_each(function()
+      vim.fn.delete(test_dir, "rf")
+      -- Restore original notify function
+      vim.notify = original_notify
+    end)
+
+    it("should watch for changes in selected env file", function()
+      local refresh_called = false
+      ecolog.refresh_env_vars = function()
+        refresh_called = true
+      end
+
+      -- Create and select env file
+      local env_file = test_dir .. "/.env"
+      vim.fn.writefile({ "KEY=value" }, env_file)
+
+      ecolog.setup({
+        path = test_dir,
+        shelter = {
+          configuration = {},
+          modules = {},
+        },
+        integrations = {},
+        types = true,
+      })
+
+      -- Edit the file and trigger write event
+      vim.cmd("edit " .. env_file)
+      vim.cmd("doautocmd BufWritePost")
+
+      -- Give time for async operations
+      vim.wait(100)
+
+      assert.is_true(refresh_called, "Should have called refresh_env_vars")
     end)
   end)
 end)
